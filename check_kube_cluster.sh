768ce432c7a0a1927643abbf0734599ef7dc473f
Eduardo Patrocinio <epatro@gmail.com>
2018-03-09 16:33:42 -0500

    First version, but it's amazing

---
+++ check_kube_cluster.sh
@@ -0,0 +1,23 @@
+echo Checking health of Pods
+
+FILE=/tmp/gp
+
+#kubectl get pods --all-namespaces > $FILE
+
+COUNT=$(wc -l $FILE)
+
+echo === Number of Pods: $COUNT
+
+echo === Pods that are not in Running state
+
+grep -v Running $FILE
+
+echo === Pods with higher number of restarts
+while read -r pod
+do
+	RESTART=$(echo $pod | awk '{print $5}')
+	if (( $RESTART > 10 ));
+	then
+		echo $pod
+	fi
+done < $FILE
