39a2758d2bfb2b0ad4d3cf33eab876b46e1e99e7
Eduardo Patrocinio <epatro@gmail.com>
2018-04-08 21:10:27 +0200

    Create restartUnhealthyPods.sh

--- restartUnhealthyPods.sh
+++ restartUnhealthyPods.sh
@@ -15,7 +15,7 @@ grep -v Running $FILE | tail +2 | tee $UNHEALTHY
 
 while read -r pod
 do
-	POD_NAME=$pod | awk '{print $2}'
+	POD_NAME="$pod | awk '{print $2}'"
 	echo Restarting $POD_NAME
 	kubectl delete po $POD_NAME
 done < $UNHEALTHY
