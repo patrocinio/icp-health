169108f030b341e83b076c377f44a9641a937759
Eduardo Patrocinio <epatro@gmail.com>
2018-04-08 21:12:44 +0200

    Create restartUnhealthyPods.sh

--- restartUnhealthyPods.sh
+++ restartUnhealthyPods.sh
@@ -2,9 +2,9 @@ echo Checking health of Pods
 
 FILE=/tmp/gp
 UNHEALTHY=/tmp/unhealthy
-alias k="kubectl -s localhost:8888"
+k="kubectl -s localhost:8888"
 
-k get pods --all-namespaces > $FILE
+$k get pods --all-namespaces > $FILE
 
 COUNT=$(wc -l $FILE)
 
@@ -18,7 +18,7 @@ while read -r pod
 do
 	POD_NAME=$(echo $pod | awk '{print $2}')
 	echo Restarting $POD_NAME
-	k delete pod $POD_NAME
+	$k delete pod $POD_NAME
 done < $UNHEALTHY
 
 
