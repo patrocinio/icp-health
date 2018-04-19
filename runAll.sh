2b0df44896b56679b0d4db792a5e5da9f01b0fc6
Eduardo Patrocinio <epatro@gmail.com>
2018-04-19 15:47:27 -0400

    Pass additional args for runAll

--- runAll.sh
+++ runAll.sh
@@ -3,4 +3,5 @@
 
 CLUSTER_DIR=/opt/icp-install/cluster
 
-ansible $1 -i $CLUSTER_DIR/hosts -e @$CLUSTER_DIR/config.yaml --private-key=$CLUSTER_DIR/ssh_key -a "$2"
+ansible $1 -i $CLUSTER_DIR/hosts -e @$CLUSTER_DIR/config.yaml --private-key=$CLUSTER_DIR/ssh_key -a "$2" ${@:3
+:99}
