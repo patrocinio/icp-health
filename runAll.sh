7815af663a3d8b7b07db5d791840710c60b20a99
Eduardo Patrocinio <epatro@gmail.com>
2018-09-17 11:54:17 -0400

    Change path to /opt/ibm

--- runAll.sh
+++ runAll.sh
@@ -1,7 +1,7 @@
 # $1 host-pattern
 # $2 command
 
-CLUSTER_DIR=/opt/icp-install/cluster
+CLUSTER_DIR=/opt/ibm/cluster
 
 ansible $1 -i $CLUSTER_DIR/hosts -e @$CLUSTER_DIR/config.yaml --private-key=$CLUSTER_DIR/ssh_key -a "$2" ${@:3
 :99}
